a998ce58c2fa8853aae20d2b1f91487b9f0bf2dc
vagrant <alx.l@gmx.com>
2014-03-17 13:22:43 +0000

    Configured gemspec

--- lib/common_domain/engine.rb
+++ lib/common_domain/engine.rb
@@ -1,5 +1,11 @@
 module CommonDomain
   class Engine < ::Rails::Engine
     isolate_namespace CommonDomain
+
+    # Generators
+    config.generators do |g|
+       g.test_framework :rspec, :fixture => true, :views => false
+       g.fixture_replacement :fabrication
+    end
   end
 end
