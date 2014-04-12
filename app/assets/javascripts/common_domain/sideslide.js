401f82d833223a0d276134a679440ae71af23b9b
vagrant <vagrant@dev>
2014-04-12 15:00:10 +0000

    Fixed sliding menus

--- app/assets/javascripts/common_domain/sideslide.js
+++ app/assets/javascripts/common_domain/sideslide.js
@@ -1,16 +1,18 @@
-
-$( document ).ready(function(){
-
-    $(".stopscroll").click(function(){
-      validate(this); 
+$(document).ready(function(){
+    $('.stopscroll').click(function(){
+      stop_scroll(this);
+      uncheck_other(this);
     });
 
-    function validate(that) {
-        if ($(that).is(':checked')){
-            $('html, body').css({
-                'overflow': 'hidden',
-                'height': '100%'
-            })
+    // Stops scrolling while menu is active, restores when unchecked
+    function stop_scroll(that) {
+        if($(that).is(':checked')){
+            if($('html, body').css('overflow').toLowerCase() != 'hidden'){
+                $('html, body').css({
+                    'overflow': 'hidden',
+                    'height': '100%'
+                }) 
+            }
         }
         else{
             $('html, body').css({
@@ -18,5 +20,32 @@ $( document ).ready(function(){
                 'height': 'auto'
             })
         }
-    } 
+    }
+
+    // Unchecks the other menu, which effectively hides it
+    function uncheck_other(that){
+        var id = $(that).attr('id');
+
+        if(id == 'right-trigger'){
+            $('#left-trigger').attr('checked', false);
+
+            active_menu(that, id, 'left-trigger');
+        }
+        else{
+            $('#right-trigger').attr('checked', false);
+
+            active_menu(that, id, 'right-trigger');
+        }
+    }
+
+    // Changes style of active menu, removes style from the other menu
+    function active_menu(that, id, other){
+        if($(that).is(':checked')){
+            $('label[for="' + id + '"]').css('background', '#000');
+            $('label[for="' + other + '"]').css('background', '');
+        }
+        else{
+            $('label[for="' + id + '"]').css('background', '');
+        }
+    }
 });
