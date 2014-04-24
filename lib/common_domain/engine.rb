11c7f9a9c832f3a749983bde3b2717f3327af08d
alxl <alx.l@gmx.com>
2014-04-24 09:40:07 +0000

    Added sanitizer gem and class

--- lib/common_domain/engine.rb
+++ lib/common_domain/engine.rb
@@ -2,6 +2,8 @@ module CommonDomain
   class Engine < ::Rails::Engine
     isolate_namespace CommonDomain
     
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Generators
     config.generators do |g|
        g.test_framework :rspec, :fixture => true, :views => false
