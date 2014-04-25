73ea0c3903bffcae9f3c264819c9f5648ed3bcc8
alxl <alx.l@gmx.com>
2014-04-25 13:55:05 +0000

    Edited helpers

--- app/helpers/common_domain/form_helper.rb
+++ app/helpers/common_domain/form_helper.rb
@@ -12,8 +12,16 @@ module CommonDomain
       end
     end
 
+    # Remove characters unsuitable for input name and id
     def remove_chars(value)
       value.gsub(/&|\s+/, "")
+    end
+
+    # Restore whitespace where needed (replaces underscore)
+    def restore_whitespace(value)
+      unless value.blank?
+        value.gsub(/_+/, " ")
+      end
     end
   end
 end
