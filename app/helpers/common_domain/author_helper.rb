72e4ddca89f993ad9ca59a110dd1a0f8ff0ce1a9
alxl <alx.l@gmx.com>
2014-05-07 22:03:58 +0000

    General bugfixing and cleanup

--- app/helpers/common_domain/author_helper.rb
+++ app/helpers/common_domain/author_helper.rb
@@ -1,8 +1,13 @@
 module CommonDomain
   module AuthorHelper
     def find_author_name(id)
-      user = Member::User.find(id)
-      user.username
+    	user = Member::User.find_by_id(id)
+
+      unless user.blank?
+      	user.username
+      else
+      	false
+      end
     end
   end
 end
