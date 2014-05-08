85c3a8d4a19219135ee5baa359b39cae9179c9a5
alxl <alx.l@gmx.com>
2014-05-08 20:22:54 +0000

    Style and helper updates

--- app/helpers/common_domain/application_helper.rb
+++ app/helpers/common_domain/application_helper.rb
@@ -1,5 +1,9 @@
 module CommonDomain
   module ApplicationHelper
+  	def main_nav_pages
+      @pages = Administration::Page.all.where(main: true).limit(3)
+    end
+
     def footer_pages
       @pages = Administration::Page.all
     end
